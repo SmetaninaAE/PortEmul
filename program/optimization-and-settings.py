028fc8dc040ba9fedaf5d56416c11374f4f1a5d4
SmetaninaAE <163336870+SmetaninaAE@users.noreply.github.com>
2024-03-20 23:28:11 +0300

    Изменена функция инициализации

---
+++ program/optimization-and-settings.py
@@ -0,0 +1,23 @@
+# Модуль управления производительностью
+def manage_performance():
+    pass
+
+# Модуль настройки
+def settings():
+    pass
+
+# Модуль сетевой игры
+def multiplayer():
+    pass
+
+# Модуль отладки
+def debug():
+    pass
+
+# Модуль синхронизации
+def synchronize():
+    pass
+
+# Модуль управления энергопотреблением
+def manage_power():
+    pass
